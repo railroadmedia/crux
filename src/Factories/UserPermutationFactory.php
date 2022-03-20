railroadmedia/crux

commit d44ae671a13f42ff9886dbf7290384bfa170d698

fixed for paused subscriptions

src/Factories/UserPermutationFactory.php
@@ -81,12 +81,27 @@ class UserPermutationFactory
      */
     public function getPermutation(User $user, string $brand): UserPermutation
     {
-        //$brand = config('railcontent.brand');
         $userId = $user->getId();
         $subscription = UserAccessService::getMembershipSubscription($userId);
+        $isPaused = false;
 
         if (!UserAccessService::isMember($userId)) {
-            return new StudentWithoutMembershipAccess($user, $brand);
+
+            // check that is not a user with a paused subscription
+            $membershipUserProduct = UserAccessService::getMembershipUserProduct();
+            $startDate = Carbon::parse($membershipUserProduct->getStartDate());
+            $startDateIsInFuture = $startDate->gt(Carbon::now());
+            if ($startDateIsInFuture) {
+                $paidUntilDate = Carbon::parse($subscription->getPaidUntil());
+                $paidUntilIsAfterStartDate = $startDate->lt($paidUntilDate);
+                if ($subscription->getIsActive() && $paidUntilIsAfterStartDate) {
+                    $isPaused = true;
+                }
+            }
+
+            if(!$isPaused){
+                return new StudentWithoutMembershipAccess($user, $brand);
+            }
         }
 
         $membershipUserProduct = UserAccessService::getMembershipUserProduct();

src/UserPermutations/UserPermutation.php
@@ -219,11 +219,8 @@ class UserPermutation
     {
         $membershipStatus = UserAccessService::getMembershipSubscriptionState($this->user->getId());
 
-        if($membershipStatus){
-            return $membershipStatus;
-        }
-
         $userProduct = UserAccessService::getMembershipUserProduct();
+        $subscription = UserAccessService::getMembershipSubscription(current_user()->getId());
 
         if (empty($subscription) && !empty($userProduct)) {
             $membershipStatus = 'non-recurring';

commit 5eda939e91627c3443147e3547b44bfc47efd10c

fix for error introduced in d44ae671a13f42ff9886dbf7290384bfa170d698

src/Factories/UserPermutationFactory.php
@@ -89,9 +89,13 @@ class UserPermutationFactory
 
             // check that is not a user with a paused subscription
             $membershipUserProduct = UserAccessService::getMembershipUserProduct();
-            $startDate = Carbon::parse($membershipUserProduct->getStartDate());
-            $startDateIsInFuture = $startDate->gt(Carbon::now());
-            if ($startDateIsInFuture) {
+
+            if($membershipUserProduct){
+                $startDate = Carbon::parse($membershipUserProduct->getStartDate());
+                $startDateIsInFuture = $startDate->gt(Carbon::now());
+            }
+
+            if ($startDateIsInFuture ?? false) {
                 $paidUntilDate = Carbon::parse($subscription->getPaidUntil());
                 $paidUntilIsAfterStartDate = $startDate->lt($paidUntilDate);
                 if ($subscription->getIsActive() && $paidUntilIsAfterStartDate) {